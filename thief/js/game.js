a994150d6330b8cbb66cfa0bb0976994853af2fc
David <david.nonamedguy@gmail.com>
2013-08-24 07:21:30 +0100

    Some stuff shows on the screen

--- thief/js/game.js
+++ thief/js/game.js
@@ -2,27 +2,89 @@
 /* jslint browser: true */
 (function Game() {
     "use strict";
-    var stage = new Kinetic.Stage({
-            container: 'container',
-            width: 578,
-            height: 200
+
+
+    var gameCanvas = document.createElement("canvas"),
+        ctx = gameCanvas.getContext('2d');
+    gameCanvas.id = "game";
+    gameCanvas.width = 640;
+    gameCanvas.height = 480;
+
+    function toXY(index, map) {
+        return {
+            x: index % map.width,
+            y: Math.floor(index / map.width)
+        };
+    }
+
+    function fromXY(x, y, map) {
+        return y * map.width + x;
+    }
+
+    function loadTileset(tileset) {
+        var img = new Image();
+        var deferred = $.Deferred();
+
+        $(img).on("load", function () {
+            deferred.resolve();
         });
-    var layer = new Kinetic.Layer();
-    var thief = new Image();
 
-    thief.onload = function () {
-        var sprite = new Kinetic.Image({
-            x: 200,
-            y: 50,
-            image: thief,
-            width: 32,
-            height: 32
+        img.src = tileset.image;
+        tileset.img = img;
+
+        deferred.promise();
+    }
+
+    function loadMap(json, callback) {
+        var map = json;
+        var tilesetLoaders = [];
+
+        _.each(map.tilesets, function (tileset) {
+            tilesetLoaders.push(loadTileset(tileset));
         });
+        
+        $.when.apply(null, tilesetLoaders).done(function () {
+            callback(map);
+        });
+    }
 
-        layer.add(sprite);
-        stage.add(layer);
-    };
+    function drawMap(map, ctx) {
+        _.each(map.layers, function (layer) {
+            for (var i = 0; i < layer.data.length; i++) {
+                var gid = layer.data[i];
+                var xy = toXY(i, map);
 
-    thief.src = "entities/thief.png";
+                var tileset = _.find(map.tilesets, function (tileset) {
+                    return tileset.firstgid <= gid;
+                });
 
+                if (tileset) {
+                    var txy = toXY(gid - tileset.firstgid, {
+                        width: tileset.imagewidth / tileset.tilewidth,
+                        height: tileset.imageheight / tileset.tileheight
+                    });
+
+                    console.log(gid, txy);
+
+                    ctx.drawImage(tileset.img,
+                        txy.x * tileset.tilewidth,
+                        txy.y * tileset.tileheight,
+                        tileset.tilewidth,
+                        tileset.tileheight,
+                        xy.x * map.tilewidth,
+                        xy.y * map.tileheight,
+                        tileset.tilewidth,
+                        tileset.tileheight);
+                }
+            }
+        });
+    }
+
+    $("div#container").append(gameCanvas);
+
+    $.getJSON("maps/demo.json", function (json) {
+        loadMap(json, function (map) {
+            drawMap(map, ctx);
+        });
+    });
 }());
